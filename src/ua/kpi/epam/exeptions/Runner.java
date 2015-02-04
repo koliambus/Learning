1327a6c1e4b2f5a299a6c6a775fb479bdebab1da
koliambus <koliambus@ukr.net>
2015-02-04 08:40:43 +0200

    world is also cruel

--- src/ua/kpi/epam/exeptions/Runner.java
+++ src/ua/kpi/epam/exeptions/Runner.java
@@ -3,7 +3,7 @@ package ua.kpi.epam.exeptions;
 public class Runner {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello my cruel  world!");
 
 	}
 
